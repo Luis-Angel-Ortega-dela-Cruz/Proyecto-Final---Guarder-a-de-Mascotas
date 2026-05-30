9ac343ed64e58238513aec8e66bb41c24a23e9fe
Audiologik <142362518+Audiologik@users.noreply.github.com>
2026-05-30 17:24:26 -0600

    Script para crear BD

---
+++ ELIMINACION_BASE_DATOS.sql
@@ -0,0 +1,13 @@
+--- eliminacion base de datos
+
+--PARA BORRAR LA BASE DE DATOS
+USE master;
+GO
+
+ALTER DATABASE [guarderia_mascotas]
+SET SINGLE_USER
+WITH ROLLBACK IMMEDIATE;
+GO
+
+DROP DATABASE [guarderia_mascotas];
+GO
