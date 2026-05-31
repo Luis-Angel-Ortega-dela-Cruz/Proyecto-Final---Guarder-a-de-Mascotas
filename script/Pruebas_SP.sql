ee26f6453b3412ef32dcbbd304388cf07f918af5
Audiologik <142362518+Audiologik@users.noreply.github.com>
2026-05-31 13:16:40 -0600

    Archivos para entrega

---
+++ script/Pruebas_SP.sql
@@ -0,0 +1,65 @@
+---Pruebas para procedimientos almacenados
+
+
+------------------------------------------------
+---Prueba de SP 4
+------------------------------------------------
+--Nueva consulta para probar:
+INSERT INTO CONSULTA (CONSULTA_ID,DIAGNOSTICO, FECHA,DETALLES,EMPLEADO_ID,MASCOTA_ID)
+VALUES(NEXT VALUE FOR seq_consulta,'Infección estomacal',GETDATE(),'Se receta medicamento y revisión posterior',1004,2);
+
+--Validar stock actual de medicamento
+select * from INVENTARIO_MEDICO where centro_id = 1;
+go
+
+--Prueba de la ejecución del sp
+EXEC sp_registra_medicamento_consulta
+    @consulta_id = 25,
+    @medicamento_id = 10,
+    @centro_id = 1,
+    @cantidad = 3;
+
+--Ver que se actualizo correctamente la consulta
+select * from INV_MED_CONSULTA im
+right join CONSULTA c on c.CONSULTA_ID = im.CONSULTA_ID
+where c.consulta_id = 25;
+
+--Ver que se redujo el stock del medicamento empleado
+select * from INVENTARIO_MEDICO where centro_id = 1;
+go
+
+------------------------------------------------
+---Prueba de SP 3
+------------------------------------------------
+DECLARE @id_consulta numeric(10,0);
+
+--Permite la creación de la consulta, y de un medicamento para dicha consulta
+EXEC sp_registra_consulta_tratamiento
+    @diagnostico = 'Infección estomacal',
+    @tratamiento = 'Administrar medicamento cada 12 horas durante 5 días.',
+    @empleado_id = 1004,
+    @mascota_id = 2,
+    @centro_id = 1,
+    @medicamento_id = 4,
+    @cantidad = 2,
+    @consulta_id = @id_consulta OUTPUT;
+
+--Para agregar más medicamentos, se empela el procedimeinto almacenado 3 (sp_registra_medicamento_consulta)
+EXEC sp_registra_medicamento_consulta
+    @consulta_id = @id_consulta,
+    @medicamento_id = 5,
+    @centro_id = 1,
+    @cantidad = 2;
+
+--Permite observar el correcto funcionamiento de los procedimeintos, y el tigger tr_actualiza_costo_consulta
+select c.CONSULTA_ID,c.DIAGNOSTICO, c.DETALLES, c.MASCOTA_ID, c.COSTO as costo_consulta, im.CANTIDAD as cantidad_medicamento,
+  inv.MEDICAMENTO_ID, im.COSTO_TOTAL as costo_medicamentos, inv.CANTIDAD as cantidad_existencias, 
+  inv.CENTRO_ID as centro_inventarioMedico
+from CONSULTA c
+join INV_MED_CONSULTA im
+on c.CONSULTA_ID = im.CONSULTA_ID
+join INVENTARIO_MEDICO inv
+on im.INV_MED_ID = inv.INV_MED_ID
+where c.CONSULTA_ID=@id_consulta;
+go
+
---
+++ script/dml.sql
@@ -0,0 +1,192 @@
+
+
+use [guarderia_mascotas]
+go
+
+
+---------------------------------------------------------------
+--TRIGGERS
+---------------------------------------------------------------
+
+--TRIGGER 1
+--Permite actualizar el precio de una consulta, sumando el precio de los medicamentos
+--incluidos en cada consulta.
+
+create trigger tr_actualiza_costo_consulta
+on INV_MED_CONSULTA
+after insert
+as
+begin
+    update CONSULTA
+    set c.COSTO = 350 + q.total_medicamentos
+    from CONSULTA c
+    inner join (
+        select imc.CONSULTA_ID, sum(imc.COSTO_TOTAL) as total_medicamentos
+        from INV_MED_CONSULTA imc
+        where imc.CONSULTA_ID in (select CONSULTA_ID
+									from inserted)
+        group by imc.CONSULTA_ID) q
+        on q.CONSULTA_ID = c.CONSULTA_ID;
+end;
+go
+
+
+---------------------------------------------------------------
+--PROCEDIMIENTOS ALMACENADOS
+---------------------------------------------------------------
+
+--PROCEDIMIENTO ALMACENADO 4
+--Permite incluir mediacamentos recetados para cada consulta, y actuliza el stock de los
+--medicamentos utilizados
+create procedure sp_registra_medicamento_consulta
+(
+    @consulta_id numeric(10,0),
+    @medicamento_id numeric(10,0),
+    @centro_id numeric(5,0),
+    @cantidad numeric(3,0)
+)
+as
+begin
+
+    declare @inv_med_id numeric(10,0),
+	  @stock_actual numeric(5,0),
+      @costo_unitario numeric(10,2),
+      @costo_total numeric(10,2);
+
+    begin try
+        begin transaction;
+
+        -- Validar que la consulta exista
+        if not exists ( select 1 from CONSULTA
+                         where CONSULTA_ID = @consulta_id)
+        begin
+            throw 50001, 'La consulta indicada no existe.', 1;
+        end;
+
+        -- Validar cantidad
+        if @cantidad <= 0
+        begin
+            throw 50002, 'La cantidad debe ser mayor a cero.', 1;
+        end;
+
+        -- Buscar inventario del medicamento en el centro
+        select 
+            @inv_med_id = im.INV_MED_ID,
+            @stock_actual = im.CANTIDAD,
+            @costo_unitario = m.COSTO
+        from INVENTARIO_MEDICO im
+        join MEDICAMENTO m
+            on m.MEDICAMENTO_ID = im.MEDICAMENTO_ID
+        where im.MEDICAMENTO_ID = @medicamento_id
+          and im.CENTRO_ID = @centro_id;
+
+        -- Validar que exista en inventario
+        if @inv_med_id is null
+        begin
+            throw 50003, 'El medicamento no existe en el inventario del centro indicado.', 1;
+        end;
+
+        -- Validar stock
+        if @stock_actual < @cantidad
+        begin
+            throw 50004, 'No hay stock suficiente del medicamento indicado.', 1;
+        end;
+
+        set @costo_total = @cantidad * @costo_unitario;
+
+        -- Validar si ese medicamento ya fue registrado en la consulta
+        if exists ( select 1 from INV_MED_CONSULTA
+                    where INV_MED_ID = @inv_med_id
+                    and CONSULTA_ID = @consulta_id)
+        begin
+            throw 50005, 'Ese medicamento ya fue registrado en esta consulta.', 1;
+        end;
+
+        -- Registrar medicamento vendido en la consulta
+        insert into INV_MED_CONSULTA (INV_MED_ID, CONSULTA_ID, CANTIDAD, COSTO_TOTAL)
+        values( @inv_med_id, @consulta_id, @cantidad, @costo_total);
+
+        -- Actualiza el stock
+        update INVENTARIO_MEDICO
+        set cantidad = cantidad - @cantidad
+        where INV_MED_ID = @inv_med_id;
+
+        commit transaction;
+    end try
+    begin catch
+        rollback transaction;
+
+        throw;
+    end catch;
+end;
+GO
+
+---PROCEDIMIENTO ALMACENADO 3
+--Permite registrar lista de medicamentos y consulta, actualizando stock para los médicamentos 
+-- incluidos en la consulta
+
+create procedure sp_registra_consulta_tratamiento
+(
+    @diagnostico varchar(400),
+    @tratamiento varchar(400) = null,
+    @empleado_id numeric(10,0),
+    @mascota_id numeric(10,0),
+
+    @centro_id numeric(5,0) = null,
+    @medicamento_id numeric(10,0) = null,
+    @cantidad numeric(3,0) = null,
+
+    @consulta_id numeric(10,0) output
+)
+as
+begin
+    begin try
+        begin transaction;
+
+        -- validar que exista el veterinario
+        if not exists (select 1 from VETERINARIO
+                       where EMPLEADO_ID = @empleado_id)
+        begin
+            throw 51001, 'El veterinario indicado no existe.', 1;
+        end;
+
+        -- validar que exista la mascota
+        if not exists (select 1 from MASCOTA 
+		               where MASCOTA_ID = @mascota_id)
+        begin
+            throw 51002, 'La mascota indicada no existe.', 1;
+        end;
+
+        -- Siguiente id de consulta
+        set @consulta_id = next value for seq_consulta;
+
+        insert into CONSULTA(CONSULTA_ID, DIAGNOSTICO, FECHA, DETALLES, EMPLEADO_ID, MASCOTA_ID)
+        values(@consulta_id, @diagnostico, getdate(), isnull(nullif(@tratamiento, ''),'Sin tratamiento indicado'), 
+		  @empleado_id, @mascota_id);
+
+        -- si hay medicamento, se llama al procedimiento que registra el medicamento y actualiza el stock
+        if @centro_id is not null
+           and @medicamento_id is not null
+           and @cantidad is not null
+        begin
+            exec sp_registra_medicamento_consulta
+                @consulta_id = @consulta_id,
+                @medicamento_id = @medicamento_id,
+                @centro_id = @centro_id,
+                @cantidad = @cantidad;
+        end;
+
+        commit transaction;
+    end try
+    begin catch
+
+        rollback transaction;
+
+        throw;
+    end catch;
+end;
+go
+
+---PROCEDIMIENTO ALMACENADO 3
+--Permite registrar lista de medicamentos y consulta, actualizando stock para los médicamentos 
+-- incluidos en la consulta
